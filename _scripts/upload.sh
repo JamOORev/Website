008b71607d3c958fd73bc3db7bfc461d631d09b8
Jackson Isted <dev@hawks008.dyndns.org>
2014-08-14 16:26:22 +1000

    dont upload gemfiles and upload basic coming soon page

--- _scripts/upload.sh
+++ _scripts/upload.sh
@@ -9,7 +9,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	if [ "${TRAVIS_BRANCH}" = "master" ]; then
 		echo "public_html"
 	else
-		echo "dev_html/`getBranch`"
+		echo "dev_html/main_site/`getBranch`"
 	fi
 else
 	echo "PR-${TRAVIS_PULL_REQUEST}-`getBranch`"
@@ -22,6 +22,6 @@ if [ "${TRAVIS_REPO_SLUG}" != "JamOORev/Website" ]; then
 fi
 
 sudo apt-get install ncftp --yes
-cd _site/ && ncftpput -R -DD -v -u $FTP_USER -p $FTP_PASSWORD jamoorev.com "`getFolder`/" ./*
+cd _site/ && rm -f Gemfile* && ncftpput -R -DD -v -u $FTP_USER -p $FTP_PASSWORD jamoorev.com "`getFolder`/" ./*
 
 #curl --ftp-create-dirs -T _site/ -u $FTP_USER:$FTP_PASSWORD ftp://jamoorev.com/`getFolder`
